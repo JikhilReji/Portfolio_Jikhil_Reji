4a450fdd2795e2bbe884da9c9487554ffb18ab2f
Jikhil Reji <98845232+theurbanmoto@users.noreply.github.com>
2025-09-27 20:53:25 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+       <Projects />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+   
+    
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,328 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "3D Developer Portfolio",
+    description:
+      "Developed a modern, interactive 3D developer portfolio that blends creativity and technology to showcase skills with immersive visuals and smooth animations.",
+    subDescription: [
+      "Built immersive 3D experiences with React Three Fiber and Drei, enhanced by Framer Motion for fluid animations and scroll-based interactions.",
+      "Designed a responsive, modern UI using TailwindCSS, with additional enhancements from Aceternity UI and Magic UI.",
+      "Implemented a fully functional contact form with EmailJS for seamless communication.",
+      "Optimized development and performance with Vite, ensuring lightning-fast builds and smooth deployment.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "React Three Fiber",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 2,
+        name: "Framer Motion",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "Vite",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "E-commerce Web App",
+    description:
+     "Developed a modern e-commerce platform with real-time product customization and integrated customer support, boosting overall user satisfaction by 35% and ensuring seamless shopping across all devices.",
+    subDescription: [
+      "Built a responsive and mobile-first UI using HTML5, CSS3, JavaScript, and Bootstrap 5, ensuring compatibility across devices and browsers.",
+      "Implemented real-time product customization features, enhancing customer engagement and personalization.",
+      "Integrated live chat support to improve customer experience and provide instant assistance",
+      "Enhanced shopping workflows with secure forms, intuitive navigation, and responsive design.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 2,
+        name: "Javascrip (ES6+)",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Material-UI (MUI)",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "CSS3",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Admin Panel",
+    description:
+      "Developed a scalable, multi-tenant admin panel with advanced access control, data visualization, and optimized backend performance.",
+    subDescription: [
+      "Designed a responsive and user-friendly interface using HTML5, CSS3, and Bootstrap 5.",
+      "Implemented secure authentication, role-based access control, and optimized backend operations using Node.js and PHP.",
+      "Managed and queried relational data efficiently with MySQL, enhancing performance and reliability.",
+      "Added real-time monitoring, analytics dashboards, and visual reporting to improve admin decision-making.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/blazor-app.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "MySQL",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "HTML5 & CSS3",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Bootstrap 5",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Dr. Finder ( Android Applcation )",
+    description:
+      "An Android application that connects users with nearby doctors in emergencies, providing real-time location tracking and optimized route guidance to improve response times.",
+    subDescription: [
+      "Developed a location-aware Android app using GPS and Google Maps API to track doctors and patients in real time.",
+      "Optimized routing algorithms to reduce emergency response times by 30%.",
+      "Integrated Firebase for real-time data synchronization and SQLite for local data management.",
+      "Implemented a clean and user-friendly interface using Android Studio and Java for seamless mobile experience.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Android Studio",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "Java & SQLite",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Firebase",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Google Map API",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Online Tutor Finding System",
+    description:
+      "Developed a web platform connecting students and parents with qualified tutors, enabling real-time interactions, demo scheduling, and online bookings while enhancing accessibility and user experience.",
+    subDescription: [
+      "Built a multi-user system with distinct roles for Admin, Parents, and Tutors, supporting profile management, course booking, and rating functionality.",
+      "Implemented real-time scheduling, demo lecture requests, and online tutor selection for enhanced learning convenience.",
+      "Designed a responsive and interactive frontend with intuitive navigation and e-book access for students.",
+      "Integrated secure backend functionality with database management for user credentials, tutor information, and booking records.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/wordpress-theme.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "PHP",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "MySQL",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "HTML5 & CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "XAMPP",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Online Learning Platform",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Azure",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer",
+    job: "Security & Defense Projects",
+    date: "2021-2023",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  {
+    title: "Back-End Developer",
+    job: "Car Manufacture",
+    date: "2023-2024",
+    contents: [
+      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
