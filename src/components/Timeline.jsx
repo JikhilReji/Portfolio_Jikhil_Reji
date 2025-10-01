21c5182f92425f0176136ec7f6cb3bd13e25f7b9
Jikhil Reji <jikhilreji@gmail.com>
2025-10-02 01:06:24 +0200

    Experience section buid

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -31,7 +31,9 @@ export const Timeline = ({ data }) => {
             key={index}
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
-            <div className="sticky z-40 flex flex-col items-center self-start max-w-xs md:flex-row top-40 lg:max-w-sm md:w-full">
+            <div className="flex-col flex z-40 items-center self-start max-w-xs md:flex-row md:sticky md:top-40 lg:max-w-sm md:w-full mt-10">
+
+
               <div className="absolute flex items-center justify-center w-10 h-10 rounded-full -left-[15px] bg-midnight">
                 <div className="w-4 h-4 p-2 border rounded-full bg-neutral-800 border-neutral-700" />
               </div>
@@ -59,7 +61,11 @@ export const Timeline = ({ data }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-1 left-1 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-1 left-1 top-0 overflow-hidden
+           w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] 
+           from-transparent from-[0%] via-neutral-700 to-transparent to-[99%]
+           [mask-image:linear-gradient
+           (to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -2,10 +2,10 @@ import { Timeline } from "../components/Timeline";
 import { experiences } from "../constants";
 const Experiences = () => {
   return (
-    <div className="w-full">
+  <div className="w-full">
       <Timeline data={experiences} />
     </div>
-  );
+);
 };
 
 export default Experiences;
